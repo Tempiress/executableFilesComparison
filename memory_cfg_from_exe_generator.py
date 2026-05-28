6a124db50e3b36cf6e628f25e1bf25905097e70e
Tempire <danil.varlamov.03@mail.ru>
2026-05-29 02:29:24 +0300

    add r2_cache for disassemble functions

--- memory_cfg_from_exe_generator.py
+++ memory_cfg_from_exe_generator.py
@@ -4,20 +4,33 @@ from typing import Dict
 
 import r2pipe
 
+import os
+import pickle
+import hashlib
+from pathlib import Path
 
 class CFGAnalyzer:
     def __init__(self):
         self.cfg_cache = {}  # Кэш для хранения CFG в памяти
         self.call_graphs = {}  # Кэш для графов вызовов
 
-    def analyze_executable(self, exe_dist: str, r2:r2pipe) -> Dict[str, dict]:
+    def analyze_executable(self, exe_dist: str, r2:r2pipe = None) -> Dict[str, dict]:
         """
         Анализирует исполняемый файл и возвращает CFG в памяти
         :param exe_dist: Путь к исполняемому файлу
         :return: Словарь с CFG {имя_функции: cfg_data}
         """
 
+        if exe_dist in self.cfg_cache:
+            return self.cfg_cache[exe_dist]
+        
+        r2_provided = r2 is not None 
         try:
+            
+            if not r2_provided:
+                r2 = r2pipe.open(exe_dist, flags=["-2", "-e io.cache=true"])                
+                r2.cmd("aaa")
+            
             # Получаем список функций
             functions = r2.cmdj("aflj")
 
@@ -52,17 +65,33 @@ class CFGAnalyzer:
 
         except Exception as e:
             logging.error(f"Error analyzing {exe_dist}: {e}")
+            
             with open(f"error_log{time.time()}.txt", "a") as f:
                 f.write(f"Error analyzing {exe_dist} for cfg_cache: {e}\n")
+            raise Exception(f"Error analyzing {exe_dist}: {e}")
+        
+        finally:
+            if not r2_provided and r2 is not None:
+                r2.quit()
 
-
-    def get_call_graph(self, exe_dist: str, r2:r2pipe) -> dict:
+    def get_call_graph(self, exe_dist: str, r2:r2pipe = None) -> dict:
+        
         """
         Получает граф вызовов и сохраняет в памяти
         :param exe_dist: Путь к исполняемому файлу
         :return: Граф вызовов в виде словаря
         """
+
+        if exe_dist in self.call_graphs:
+            return self.call_graphs[exe_dist]
+
+        r2_provided = r2 is not None
+         
         try:
+
+            if not r2_provided:
+                r2 = r2pipe.open(exe_dist, flags=["-2", "-e io.cache=true"])
+                r2.cmd("aaa")
 
             # Получаем граф вызовов
             call_graph =r2.cmdj("agCj")
@@ -77,9 +106,38 @@ class CFGAnalyzer:
             logging.error(f"Error getting call graph for {exe_dist}: {e}")
             with open(f"error_log{time.time()}.txt", "a") as f:
                 f.write(f"Error analyzing {exe_dist} for call_graphs: {e}\n")
+            raise Exception(f"Error getting call graph for {exe_dist}: {e}")
+        finally:
+            if not r2_provided and r2 is not None:
+                r2.quit()
 
 
     def get_analyzers(self, exe_dist: str) -> dict:
+        cache_dir = Path(__file__).parent / ".r2_cache"
+        cache_dir.mkdir(exist_ok=True)
+
+        abs_path = os.path.abspath(exe_dist)
+        path_hash = hashlib.md5(abs_path.encode("utf-8")).hexdigest()
+
+        mtime = int(os.path.getmtime(abs_path))
+        size = os.path.getsize(abs_path)
+
+        cache_file = cache_dir / f"cache_{path_hash}_{mtime}_{size}.pkl"
+        
+        if cache_file.exists():
+            try:
+                with open(cache_file, "rb") as f:
+                    data = pickle.load(f)
+                    self.cfg_cache[exe_dist] = data["cfg_data"]
+                    self.call_graphs[exe_dist] = data["call_graph"]
+                    return self.cfg_cache[exe_dist], self.call_graphs[exe_dist]
+
+            except Exception as e:
+                logging.warning(f"Cache file {cache_file} is corrupted: {e}")
+                cache_file.unlink()
+
+        
+        
         if exe_dist not in self.cfg_cache or exe_dist not in self.call_graphs:
             try: 
                 r2 = r2pipe.open(exe_dist, flags=["-2", "-e io.cache=true"])
@@ -90,6 +148,15 @@ class CFGAnalyzer:
 
                 if exe_dist not in self.call_graphs:
                     self.get_call_graph(exe_dist, r2)
+
+                data_to_cache = {
+                    "cfg_data": self.cfg_cache[exe_dist],
+                    "call_graph": self.call_graphs[exe_dist]
+                }
+                temp_file = cache_file.with_suffix('.tmp')
+                with open(temp_file, "wb") as f:
+                    pickle.dump(data_to_cache, f, protocol= pickle.HIGHEST_PROTOCOL)
+                os.replace(temp_file, cache_file)
 
             except Exception as e:
                 logging.error(f"Error getting analyzers for {exe_dist}: {e}")
